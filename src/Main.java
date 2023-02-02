815bba2da4fb4c20bf95ec4568a3f9f447cbbe85
Роман <roma_yakushin@mail.ru>
2023-02-02 17:15:03 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        int a = 1545657;
+        byte b = 3;
+        short c = 100;
+        long d = 109834;
+        float e = 100.67F;
+        double f = 5.5;
+        System.out.println("Значение переменной 'а' с типом int равно " + a);
+        System.out.println("Значение переменной 'b' с типом byte равно " + b);
+        System.out.println("Значение переменной 'c' с типом short равно " + c);
+        System.out.println("Значение переменной 'd' с типом long равно " + d);
+        System.out.println("Значение переменной 'e' с типом float равно " + e);
+        System.out.println("Значение переменной 'f' с типом double равно " + f);
     }
 }
